c9ca7270fb9a6cfac81b459fc5cea5c76fa32a82
adnan <AdnanGhannam2001>
2024-04-07 12:23:14 +0300

    Add IServiceCollection Extension

---
+++ src/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,44 @@
+using System.Reflection;
+using ChatService.Data;
+using ChatService.Interfaces;
+using ChatService.Services;
+using MassTransit;
+
+namespace ChatService.Extensions;
+
+internal static class IServiceCollectionExtensions {
+    public static IServiceCollection AddAuth(this IServiceCollection services) {
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.Development.json")
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var cookies = config["Cookies"] ?? throw new Exception("`Cookies` should be defined in `appsettings.json`");
+
+        services
+            .AddAuthentication(cookies)
+            .AddCookie(cookies);
+
+        services.AddAuthorization();
+
+        return services;
+    }
+
+    public static IServiceCollection AddRabbitMQ(this IServiceCollection services) {
+        return services.AddMassTransit(config => {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            config.RegisterConsumersFromAssembly(assembly);
+
+            config.UsingRabbitMq((context, rmq) => {
+                rmq.ConfigurationReceiveEndpointsFromAssembly(assembly, context);
+            });
+        });
+    }
+
+    public static IServiceCollection RegisterServices(this IServiceCollection services) {
+        return services
+                .AddScoped<DapperDbConnection>()
+                .AddScoped<IChatsService, ChatsService>();
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,46 +1,17 @@
 using System.Security.Claims;
-using ChatService.Consumers;
 using ChatService.Data;
 using ChatService.Endpoints;
-using ChatService.Interfaces;
-using ChatService.Services;
-using MassTransit;
-using MassTransit.Configuration;
+using ChatService.Extensions;
 using Microsoft.AspNetCore.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var cookies = builder.Configuration["Cookies"] ?? throw new Exception("`Cookies` should be defined in `appsettings.json`");
-
-builder.Services.AddAuthentication(cookies)
-    .AddCookie(cookies);
-builder.Services.AddAuthorization();
-
-builder.Services.AddMassTransit(config => {
-    config.RegisterConsumer<GroupCreatedEventConsumer>();
-    config.RegisterConsumer<GroupDeletedEventConsumer>();
-    config.RegisterConsumer<FriendshipCreatedEventConsumer>();
-    config.RegisterConsumer<FriendshipDeletedEventConsumer>();
-    config.RegisterConsumer<MemberJoinedEventConsumer>();
-    config.RegisterConsumer<MemberLeavedEventConsumer>();
-    config.RegisterConsumer<MemberRoleChangedEventConsumer>();
-
-    config.UsingRabbitMq((context, rmqConfig) => {
-        rmqConfig.ReceiveEndpoint("group-created-event", e => e.ConfigureConsumer<GroupCreatedEventConsumer>(context));
-        rmqConfig.ReceiveEndpoint("group-deleted-event", e => e.ConfigureConsumer<GroupCreatedEventConsumer>(context));
-        rmqConfig.ReceiveEndpoint("friendship-created-event", e => e.ConfigureConsumer<FriendshipCreatedEventConsumer>(context));
-        rmqConfig.ReceiveEndpoint("friendship-deleted-event", e => e.ConfigureConsumer<FriendshipDeletedEventConsumer>(context));
-        rmqConfig.ReceiveEndpoint("member-joined-event", e => e.ConfigureConsumer<MemberJoinedEventConsumer>(context));
-        rmqConfig.ReceiveEndpoint("member-leaved-event", e => e.ConfigureConsumer<MemberLeavedEventConsumer>(context));
-        rmqConfig.ReceiveEndpoint("member-role-changed-event", e => e.ConfigureConsumer<MemberRoleChangedEventConsumer>(context));
-    });
-});
-
-builder.Services.AddScoped<DapperDbConnection>();
-builder.Services.AddScoped<IChatsService, ChatsService>();
+builder.Services
+    .AddEndpointsApiExplorer()
+    .AddSwaggerGen()
+    .AddAuth()
+    .AddRabbitMQ()
+    .RegisterServices();
 
 var app = builder.Build();
 
@@ -76,6 +47,8 @@ app.UseAuthorization();
 
 // Mock Login
 app.MapGet("/login/{id}", async (string id, HttpContext context) => {
+    var cookies = app.Configuration["Cookies"];
+
     var claim = new Claim(ClaimTypes.NameIdentifier, id);
 
     var identity = new ClaimsIdentity([claim], cookies);
